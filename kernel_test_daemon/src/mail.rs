e3421da882680bf567fa4876b243490aadce13fd
foxhlchen <foxhlchen@gmail.com>
2021-04-06 17:34:07 +0800

    add new mail interface

--- kernel_test_daemon/src/mail.rs
+++ kernel_test_daemon/src/mail.rs
@@ -1,5 +1,71 @@
 use log::{error, warn, info, debug, trace};
 
+struct MailMgr {
+	session: imap::Session<native_tls::TlsStream<std::net::TcpStream>>,
+}
+
+impl MailMgr {
+	pub fn new(imap_conf: &super::cfg::IMap) -> imap::error::Result<Self> {
+		let domain = &*imap_conf.domain;
+		let tls = native_tls::TlsConnector::builder().build().unwrap();
+		let client = imap::connect((domain, 993), domain, &tls).unwrap();
+		
+		let mut imap_session = client
+			.login(&imap_conf.username, &imap_conf.password)
+			.map_err(|e| e.0)?;
+
+		Ok(MailMgr {session: imap_session})
+	}
+
+	pub fn list(&mut self) -> imap::error::Result<Vec<String>> {
+		let list = self.session.list(None, Some("*"))?;
+		let mut vec = Vec::new();
+
+		for name in list.iter() {
+			vec.push(name.name().to_string());
+		}
+
+		Ok(vec)
+	}
+
+	pub fn fetch_mail(&mut self, seq: u32)  -> imap::error::Result<String> {
+		let messages = self.session.fetch(seq.to_string(), "RFC822")?;
+		for message in messages.iter() {
+			// extract the message's body
+			let body = message.body().expect("message did not have a body!");
+			let body = std::str::from_utf8(body)
+				.expect("message was not valid utf-8")
+				.to_string();
+			return Ok(body);
+		}
+		Err(imap::Error::Bad("No mails found!".to_string()))
+	}
+
+	pub fn fetch_mails(
+		&mut self, 
+		seqs: std::collections::HashSet<u32>
+	) -> imap::error::Result<Vec<(u32, String)>> {
+		let mut vec = Vec::new();
+		for seq in seqs {
+			let messages = self.session.fetch(seq.to_string(), "RFC822")?;
+			for message in messages.iter() {
+				// extract the message's body
+				let body = message.body().expect("message did not have a body!");
+				let body = std::str::from_utf8(body)
+					.expect("message was not valid utf-8")
+					.to_string();
+			
+				vec.push((seq, body));
+			}
+		}
+		
+		Ok(vec)
+	}
+
+	pub fn fetch_unread(&mut self) -> imap::error::Result<std::collections::HashSet<u32>> {
+		self.session.search("NOT SEEN")
+	}
+}
 
 pub fn list(imap_conf: &super::cfg::IMap) -> imap::error::Result<Vec<String>> {
 	let domain = &*imap_conf.domain;
@@ -59,3 +125,17 @@ pub fn fetch_unread(imap_conf: &super::cfg::IMap) -> imap::error::Result<Vec<(u3
 	Ok(vec)
 }
 
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_fetch() {
+		let conf = crate::cfg::ConfigMgr::new().unwrap();
+        let mails = super::fetch_unread(&conf.get().imap).unwrap();
+
+		for (seq, mail) in mails {
+			println!("{} {}", seq, mail);
+		}
+    }
+
+}
